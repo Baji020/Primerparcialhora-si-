faf4f619a5b69dff25aac75ac59e401b88fb572f
Baji020 <145026343+Baji020@users.noreply.github.com>
2023-09-21 08:00:50 -0600

    Cod_olimpiadas1.py

---
+++ Cod_olimpiadas1.py
@@ -0,0 +1,31 @@
+def suma(a,b):
+  x = a+ b
+  return x
+
+def resta(a,b):
+  x = a - b
+  return x
+
+print("Dame elcprimer numero:")
+a = int(input())
+print("D ae el segundo numero:")
+b = int(input())
+print("La suma de", suma(a,b),"y la resta de", resta(a,b))
+
+
+
+
+
+def multiplicacion(a,b):
+x=a*b
+return x
+
+def division(a,b):
+  x=a/b
+  return x
+
+print("Dame el primer punto:")
+a=(int(input())
+print("D ae el segundo numero:")
+b = int(input())
+print("La multiplicacion de", multiplicacion(a,b),"y la division de", division(a,b))
